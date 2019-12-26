4349c69c25b05a0a0c1c72486b96e7c89b1627b9
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2019-12-26 16:43:53 +0100

    Playing with JDBC and sql driver

---
+++ src/jdbc/testJDBC.java
@@ -0,0 +1,32 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * Created on 12/26/2019.
+ */
+public class testJDBC {
+
+  private static final Logger logger = Logger.getLogger(testJDBC.class.getName());
+
+  public static void main(String[] args) throws Exception {
+    Statement st;
+    Connection cx = DriverManager.getConnection("jdbc:postgresql://localhost/mydb", System.getenv("LOGIN"), System.getenv("PWD"));
+
+    st = cx.createStatement();
+
+    ResultSet rs = st.executeQuery("SELECT * FROM persons");
+
+    while (rs.next()) {
+      String lastname = rs.getString("lastname");
+      int personid = rs.getInt("personid");
+      logger.log(Level.INFO, String.format("personid : %s ,  lastname : %s ", personid, lastname));
+    }
+
+  }
+}
