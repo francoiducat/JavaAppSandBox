2f21688ed8342419d542f9a0307b07172f87e2e0
Francois DUCATILLON <francois.ducatillon@decathlon.com>
2020-01-21 12:45:27 +0100

    playing with Map and TreeMap

---
+++ src/main/java/map/playingWithMap.java
@@ -0,0 +1,42 @@
+package map;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ * Created on 01/20/2020.
+ */
+public class playingWithMap {
+
+  public static void main(String args[]) {
+//    playingWithMap();
+    playingWithTreeMap();
+  }
+
+  private static void playingWithMap() {
+
+    Map<Integer, String> myMap = new HashMap<Integer, String>();
+
+    myMap.put(1, "entry one");
+    myMap.put(2, "entry two");
+    myMap.put(3, "entry three");
+
+    myMap.entrySet().stream().sorted(Map.Entry.comparingByKey(Comparator.reverseOrder())).forEach(System.out::println);
+    myMap.entrySet().stream().sorted(Map.Entry.comparingByKey()).forEach(System.out::println);
+  }
+
+  private static void playingWithTreeMap() {
+
+    TreeMap<Integer, String> treeMap = new TreeMap<Integer, String>();
+
+    treeMap.put(2, "entry two");
+    treeMap.put(3, "entry three");
+    treeMap.put(1, "entry one");
+
+    treeMap.entrySet().forEach(System.out::println);
+  }
+
+
+}
