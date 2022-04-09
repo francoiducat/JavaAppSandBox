f0cbd27557e13aa01435b67a5ab186764ec4fc74
francoiducat <francois.ducatillon@decathlon.com>
2022-04-09 11:48:32 +0200

    playing with javac and java commands. playing with java PATH and CLASSPATH

---
+++ src/main/java/net/ducatillon/javasandbox/cli/Zoo.java
@@ -0,0 +1,28 @@
+package net.ducatillon.javasandbox.cli;
+
+/*
+
+ # STEP 1 : Create class File
+
+   1. Go to /JavaAppSandBox/src/main/java/net/ducatillon/javasandbox/cli
+   2. Enter the command `javac Zoo.java`
+
+ # STEP 2 : Run class File
+
+   Option 1:
+   1. Go to /JavaAppSandBox/src/main/java/net/ducatillon/javasandbox/cli
+   2. Enter the command `java -classpath ../../../.. net.ducatillon.javasandbox.cli.Zoo arg1`
+
+   Option 2:
+   1. Go to /JavaAppSandBox/src/main/java
+   2. Enter the command `java net.ducatillon.javasandbox.cli.Zoo arg1`
+
+ */
+
+public class Zoo {
+
+  public static void main(String[] args) {
+    System.out.println("Zoo Entry point : " + args[0]);
+  }
+}
+
