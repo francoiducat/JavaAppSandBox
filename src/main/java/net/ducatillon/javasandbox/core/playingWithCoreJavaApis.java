bdde30c92860dbeb3311d336ff676308609c44bd
francoiducat <francois.ducatillon@decathlon.com>
2022-05-12 18:13:30 +0200

    playing with ArrayList

--- src/main/java/net/ducatillon/javasandbox/core/playingWithCoreJavaApis.java
+++ src/main/java/net/ducatillon/javasandbox/core/playingWithCoreJavaApis.java
@@ -1,14 +1,12 @@
 package net.ducatillon.javasandbox.core;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class playingWithCoreJavaApis {
 
-    public static void main(String[] args) {
-        playingWithString();
-        playingWithStringBuilder();
-        playingWithArraysList();
-    }
+    static int[] var;
 
     public static void playingWithString() {
         String str = "animals";
@@ -31,8 +29,16 @@ public class playingWithCoreJavaApis {
         System.out.println("replace(1,100, \"b\"): " + new StringBuilder("animals").replace(1, 100, "b"));
     }
 
+    public static void main(String[] args) {
+        playingWithArrays();
+        playingWithString();
+        playingWithStringBuilder();
+        playingWithArraysList();
+    }
+
     public static void playingWithArrays() {
-        int[] var;
+
+        // System.out.println("var.length" +  var.length); // throws null pointer exception
         int[] values = new int[]{1, 2, 3};
         int[] myarray = {1, 2, 3};
         //int [] ids, types;
@@ -41,15 +47,35 @@ public class playingWithCoreJavaApis {
         int index2 = values[2];
         String[] names = new String[2];
         int[][] differentSizes = {{1, 4}, {3}, {9, 8, 7}};
+
+        List<Integer> list = Arrays.asList(1);
+        //list.add(2); // Compiles but fails at run time
+
     }
 
     public static void playingWithArraysList() {
-        ArrayList list1 = new ArrayList();
         ArrayList list2 = new ArrayList(1);
         list2.add(2);
         list2.add(3);
         System.out.println("list2: " + list2);
         Integer integer = Integer.valueOf("2");
         int parseInt = Integer.parseInt("1");
+        List<String> strings = new ArrayList<>();
+        strings.add("a");
+        strings.add("b");
+        System.out.println("list2.toString(): " + list2.toString());
+        System.out.println("strings.toString(): " + strings.toString());
+
+        var list = new ArrayList<String>();
+        list.add("one");
+        list.add("two");
+        //list.add(7); //
+
+        List<Integer> ages = new ArrayList<>();
+        ages.add(Integer.parseInt("5"));
+        ages.add(Integer.valueOf("6"));
+        ages.add(7);
+        ages.add(null);
+        //  (int age : ages) System.out.print(age); // null pointer
     }
 }
