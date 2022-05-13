54cf6ff3d797b8bb00e8c6d7155d170bfed5973a
francoiducat <francois.ducatillon@decathlon.com>
2022-05-13 10:05:29 +0200

    playing with List.of()

--- src/main/java/net/ducatillon/javasandbox/core/playingWithCoreJavaApis.java
+++ src/main/java/net/ducatillon/javasandbox/core/playingWithCoreJavaApis.java
@@ -8,6 +8,14 @@ public class playingWithCoreJavaApis {
 
     static int[] var;
 
+    public static void main(String[] args) {
+        playingWithListOfArraysAsList();
+        playingWithArrays();
+        playingWithString();
+        playingWithStringBuilder();
+        playingWithArraysList();
+    }
+
     public static void playingWithString() {
         String str = "animals";
         System.out.println("str.length(): " + str.length());
@@ -18,10 +26,7 @@ public class playingWithCoreJavaApis {
         boolean var = "ABC".startsWith("a");
         System.out.println("\"ABC\".startsWith(\"a\") : " + var);
         System.out.println("new String(): " + "_" + new String() + "_");
-
-
     }
-
     public static void playingWithStringBuilder() {
         StringBuilder strbuilder = new StringBuilder("animals");
         System.out.println("strbuilder.length(): " + strbuilder.length());
@@ -29,15 +34,7 @@ public class playingWithCoreJavaApis {
         System.out.println("replace(1,100, \"b\"): " + new StringBuilder("animals").replace(1, 100, "b"));
     }
 
-    public static void main(String[] args) {
-        playingWithArrays();
-        playingWithString();
-        playingWithStringBuilder();
-        playingWithArraysList();
-    }
-
     public static void playingWithArrays() {
-
         // System.out.println("var.length" +  var.length); // throws null pointer exception
         int[] values = new int[]{1, 2, 3};
         int[] myarray = {1, 2, 3};
@@ -49,8 +46,16 @@ public class playingWithCoreJavaApis {
         int[][] differentSizes = {{1, 4}, {3}, {9, 8, 7}};
 
         List<Integer> list = Arrays.asList(1);
-        //list.add(2); // Compiles but fails at run time
+        System.out.println("list: " + list);
+        //list.add(2); // Compiles but fails at run time, throws UnsupportedOperationException
+        list.set(0, 2); // OK, mutable list
+        System.out.println("list after set: " + list);
 
+        List<Integer> integers = List.of(1);
+        System.out.println("List.of(1): " + integers);
+        //integers.add(2); // Immutable Collection, throws UnsupportedOperationException
+        //integers.set(0,3);
+        System.out.println("List.of(1) after set" + integers);
     }
 
     public static void playingWithArraysList() {
@@ -69,13 +74,34 @@ public class playingWithCoreJavaApis {
         var list = new ArrayList<String>();
         list.add("one");
         list.add("two");
-        //list.add(7); //
+        //list.add(7); // Does not compile.
 
         List<Integer> ages = new ArrayList<>();
         ages.add(Integer.parseInt("5"));
         ages.add(Integer.valueOf("6"));
         ages.add(7);
         ages.add(null);
-        //  (int age : ages) System.out.print(age); // null pointer
+        //  (int age : ages) System.out.print(age); // Compiles, throws null pointer.
+    }
+
+    public static void playingWithListOfArraysAsList() {
+
+        // Arrays.asList allows null elements while List.of doesn't:
+        List<Integer> list = Arrays.asList(1, 2, 3);
+        list.contains(null); // Returns false
+
+        List<Integer> otherList = List.of(1, 2, 3);
+        // otherList.contains(null); // Fails with NullPointerException
+
+        // Arrays.asList returns a view of the passed array, so the changes to the array will be reflected in the list too.
+        Integer[] array = {1, 2, 3};
+        List<Integer> list1 = Arrays.asList(array);
+        array[1] = 10;
+        System.out.println(list1); // Prints [1, 10, 3]
+
+        Integer[] array1 = {1, 2, 3};
+        List<Integer> list2 = List.of(array);
+        array1[1] = 10;
+        System.out.println(list2); // Prints [1, 2, 3]
     }
 }
